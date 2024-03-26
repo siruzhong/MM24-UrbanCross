b2fdc09c7b4f0b219688445deda41b784385f5f0
siruzhong <1412578784@qq.com>
2024-03-27 00:40:05 +0800

    remove useless file

--- test.sh
+++
@@ -1,5 +0,0 @@
-export TF_ENABLE_ONEDNN_OPTS=0
-datename=$(date +%Y%m%d-%H%M%S)
-python test_single.py \
-       --resume checkpoint/rsitmd/SWAN/SWAN_best.pth.tar \
-       |& tee outputs/logs_test_$datename.txt 2>&1
