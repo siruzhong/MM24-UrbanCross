057ef0e14cb592a209343ceddb55db5e5e010838
siruzhong <1412578784@qq.com>
2024-03-27 10:10:45 +0800

    change train_urbancross log files name

--- train_urbancross_rsicd.sh
+++ train_urbancross_rsicd.sh
@@ -1,16 +1,19 @@
 datename=$(date +%Y%m%d-%H%M%S)
-# RSICD Dataset
+data_name=rsicd
+epochs=35
+num_seg=4
+
 python train_urbancross.py \
        --gpuid 0 \
        --model_name ours \
-       --experiment_name urbancross \
+       --experiment_name urbancross_$datename \
        --ckpt_save_path outputs/checkpoints/ \
-       --epochs 35 \
+       --epochs $epochs \
        --image_path /hpc2hdd/home/szhong691/zsr/projects/dataset/RSICD/images \
        --country "" \
        --batch_size 40 \
-       --num_seg 4 \
+       --num_seg $num_seg \
        --workers 0 \
-       --data_name rsicd \
-       --test_step 35 \
-       2>&1 | tee -a outputs/logs_$datename.txt
+       --data_name $data_name \
+       --test_step $epochs \
+       2>&1 | tee -a outputs/logs_${datename}_data${data_name}_epochs${epochs}_seg${num_seg}.txt
--- train_urbancross_rsitmd.sh
+++ train_urbancross_rsitmd.sh
@@ -1,16 +1,19 @@
 datename=$(date +%Y%m%d-%H%M%S)
-# RSITMD Dataset
+data_name=rsitmd
+epochs=35
+num_seg=4
+
 python train_urbancross.py \
        --gpuid 0 \
        --model_name ours \
        --experiment_name urbancross \
        --ckpt_save_path outputs/checkpoints/ \
-       --epochs 35 \
+       --epochs $epochs \
        --image_path /hpc2hdd/home/szhong691/zsr/projects/dataset/RSITMD/images \
        --country "" \
        --batch_size 40 \
-       --num_seg 4 \
+       --num_seg $num_seg \
        --workers 0 \
-       --data_name rsitmd \
-       --test_step 35 \
-       2>&1 | tee -a outputs/logs_$datename.txt
+       --data_name $data_name \
+       --test_step $epochs \
+       2>&1 | tee -a outputs/logs_${datename}_data${data_name}_epochs${epochs}_seg${num_seg}.txt
