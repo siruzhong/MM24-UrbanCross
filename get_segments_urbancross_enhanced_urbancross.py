f485f01763baecf9b529e55d790db33b92e70ca7
siruzhong <1412578784@qq.com>
2024-04-05 22:01:32 +0800

    remove useless files

--- get_segments_urbancross_enhanced_urbancross.py
+++ get_segments_urbancross_enhanced_urbancross.py
@@ -110,8 +110,8 @@ def show_masks_mine(anns, ori_img, img_path, description):
 
 if __name__ == "__main__":
     img_path = "/hpc2hdd/home/szhong691/zsr/projects/dataset/UrbanCross/image_target/Finland/images"
-    # img_path = "/hpc2hdd/home/szhong691/zsr/projects/dataset/UrbanCross/image_target/Germany/images"
     df = pd.read_csv("/hpc2hdd/home/szhong691/zsr/projects/dataset/UrbanCross/image_target/Finland/instructblip_generation_finland_refine.csv")
+    # img_path = "/hpc2hdd/home/szhong691/zsr/projects/dataset/UrbanCross/image_target/Germany/images"
     # df = pd.read_csv("/hpc2hdd/home/szhong691/zsr/projects/dataset/UrbanCross/image_target/Germany/instructblip_generation_germany_refine.csv")
 
     sam_checkpoint = "sam_vit_h_4b8939.pth"
@@ -121,6 +121,7 @@ if __name__ == "__main__":
     mask_generator = SamAutomaticMaskGenerator(sam)
 
     img_lists = df["image_name"]
+    # for idx, row in tqdm(reversed(list(df.iterrows())[10000:]), total=df.shape[0]):
     for idx, row in tqdm(list(df.iterrows())[20000:], total=df.shape[0]):
         image_name = row['image_name']
         description = row['description']  # 确保CSV中有描述的列
